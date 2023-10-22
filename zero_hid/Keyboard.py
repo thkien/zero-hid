eff105fe82a64ce946c397ecfa44430765d6255f
Tran Hong Kien <Kien.TranHong@elektrobit.com>
2023-10-22 13:02:33 +0900

    Open and close HID device in Keyboard.py and Mouse.py

--- zero_hid/Keyboard.py
+++ zero_hid/Keyboard.py
@@ -11,11 +11,24 @@ import os
 import pathlib
 
 class Keyboard:
-    
-    def __init__(self, dev='/dev/hidg0') -> None:
-        self.dev = dev
+    HID_DEV=None
+
+    def __init__(self, dev_path='/dev/hidg0') -> None:
+        self.dev_path = dev_path
+
+        if Keyboard.HID_DEV:
+            self.dev = Keyboard.HID_DEV
+        else:
+            try:
+                Keyboard.HID_DEV=open(dev_path, 'ab+')
+                self.dev = Keyboard.HID_DEV
+            except Exception as e:
+                self.dev = None
+                print(f"Failed to open HID device: '{dev_path}'")
+                print(e)
+
         self.set_layout()
-    
+
     def list_layout(self):
         keymaps_dir = pathlib.Path(__file__).parent.absolute() / 'keymaps'
         keymaps = os.listdir(keymaps_dir)
@@ -24,11 +37,12 @@ class Keyboard:
             with open(keymaps_dir / fname , encoding='UTF-8') as f:
                 content = json.load(f)
                 name, desc = content['Name'], content['Description']
-            print(f'{count}. {name}: {desc}') 
-        
-        
+            print(f'{count}. {name}: {desc}')
+
+
     def set_layout(self,  language='US'):
         self.layout = json.loads( pkgutil.get_data(__name__, f"keymaps/{language}.json").decode() )
+
     def type(self, text, delay=0):
         for c in text:
             key_map = self.layout['Mapping'][c]
@@ -37,22 +51,44 @@ class Keyboard:
             keys = key_map['Keys']
             mods = [KeyCodes[i] for i in mods]
             keys = [KeyCodes[i] for i in keys]
-            
+
             if len(mods) == 1:
                 mods = mods[0]
             else:
                 mods = reduce(operator.and_, mods, 0)
-            send_keystroke(self.dev, mods, keys[0])
-            sleep(delay)
+
+            if self.dev:
+                send_keystroke(self.dev, mods, keys[0])
+                sleep(delay)
 
     def press(self, mods: List[int], key_code: int = 0, release=True):
         if len(mods) == 1:
             mods = mods[0]
         else:
             mods = reduce(operator.and_, mods, 0)
-        send_keystroke(self.dev, mods, key_code, release=release)
+
+        if self.dev:
+            send_keystroke(self.dev, mods, key_code, release=release)
 
     def release(self):
-        release_keys(self.dev)
-    
+        if self.dev:
+            release_keys(self.dev)
+
+    def _clean_resources(self):
+        if Keyboard.HID_DEV:
+            Keyboard.HID_DEV.close()
+            self.dev = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._clean_resources()
+
+    def __del__(self):
+        self._clean_resources()
+
+    def close(self):
+        self._clean_resources()
+
 
--- zero_hid/Mouse.py
+++ zero_hid/Mouse.py
@@ -7,42 +7,57 @@ class RelativeMoveRangeError(Exception):
 
 
 class Mouse:
+    HID_DEV=None
+
     def __init__(self, dev_path='/dev/hidg1') -> None:
-        self.dev = open(dev_path, 'ab+')
-    
+        self.dev_path = dev_path
+
+        if Mouse.HID_DEV:
+            self.dev = Mouse.HID_DEV
+        else:
+            try:
+                Mouse.HID_DEV=open(dev_path, 'ab+')
+                self.dev = Mouse.HID_DEV
+            except Exception as e:
+                self.dev = None
+                print(f"Failed to open HID device: '{dev_path}'")
+                print(e)
+
     def left_click(self):
-        send_mouse_event(self.dev, 0x1, 0, 0, 0, 0)
-        send_mouse_event(self.dev, 0x0, 0, 0, 0, 0)
-    
+        if self.dev:
+            send_mouse_event(self.dev, 0x1, 0, 0, 0, 0)
+            send_mouse_event(self.dev, 0x0, 0, 0, 0, 0)
+
     def right_click(self):
-        send_mouse_event(self.dev, 0x1, 0, 0, 0, 0)
-        send_mouse_event(self.dev, 0x2, 0, 0, 0, 0)
-    
+        if self.dev:
+            send_mouse_event(self.dev, 0x1, 0, 0, 0, 0)
+            send_mouse_event(self.dev, 0x2, 0, 0, 0, 0)
+
     def move_relative(self, x, y):
         """
         move the mouse in relative mode
         x,y should be in range of -127 to 127
         """
-        if not -127 <= x <= 127: 
+        if not -127 <= x <= 127:
             raise RelativeMoveRangeError(f"Value of x: {x} out of range (-127 - 127)")
         if not -127 <= y <= 127:
             RelativeMoveRangeError(f"Value of x: {y} out of range (-127 - 127)")
-        send_mouse_event(self.dev, 0x0, x, y, 0, 0)
-        
-        
-        
+        if self.dev:
+            send_mouse_event(self.dev, 0x0, x, y, 0, 0)
+
+    def _clean_resources(self):
+        if Mouse.HID_DEV:
+            Mouse.HID_DEV.close()
+            self.dev = None
+
     def __enter__(self):
         return self
 
-
-    def _clean_resources(self):
-        print(f'closing handle {self.dev}')
-        self.dev.close()    
-    
     def __exit__(self, exc_type, exc_val, exc_tb):
         self._clean_resources()
-        
-        
-    
+
+    def __del__(self):
+        self._clean_resources()
+
     def close(self):
         self._clean_resources()
